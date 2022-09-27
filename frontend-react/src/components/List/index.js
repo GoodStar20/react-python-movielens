5b08f77a0e00b18f1bff778cbb99de04ffe98132
GoodStar20 <39380399+GoodStar20@users.noreply.github.com>
2022-09-26 23:38:13 -0400

    Updated List component

--- frontend-react/src/components/List/index.js
+++ frontend-react/src/components/List/index.js
@@ -1,19 +1,40 @@
 import React from "react";
 
-import Loading from "../Loading";
 import MovieCard from "../MovieCard";
 import "./styles.scss";
 
-const MovieList = ({ loading, data }) => (
-  <div>
-    <div className="row">
-      {data.length > 0 ? (
-        data.map((movie, index) => <MovieCard movie={movie} key={index} />)
-      ) : (
-        <div>There is no result</div>
-      )}
-    </div>
-    {loading && <Loading />}
+const MovieList = ({ data, currentPage }) => (
+  <div className="movie-list">
+    <table className="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Movie ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">Genres</th>
+          <th scope="col">Number of Tags</th>
+          <th scope="col">Tags</th>
+          <th scope="col">Number of Ratings</th>
+          <th scope="col">Avergage Rating</th>
+          <th scope="col">Links</th>
+        </tr>
+      </thead>
+      <tbody data-testid="table-tbody">
+        {data.length > 0 ? (
+          data.map((movie, index) => (
+            <MovieCard
+              movie={movie}
+              key={index}
+              rowId={(currentPage - 1) * 20 + (index + 1)}
+            />
+          ))
+        ) : (
+          <tr>
+            <td colSpan={9}>There is no result</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
   </div>
 );
 
